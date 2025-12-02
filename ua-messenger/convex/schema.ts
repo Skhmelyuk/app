a5347d09115d8264d48095d21b61ad6772a39cc1
Skhmelyuk <skhmelyuk1985@gmail.com>
2025-12-02 14:19:15 +0200

    lesson-09

--- ua-messenger/convex/schema.ts
+++ ua-messenger/convex/schema.ts
@@ -10,7 +10,7 @@ export default defineSchema({
     image: v.string(),
     followers: v.number(),
     following: v.number(),
-    post: v.number(),
+    posts: v.number(),
     clerkId: v.string(),
   }).index("by_clerk_id", ["clerkId"]),
   posts: defineTable({
